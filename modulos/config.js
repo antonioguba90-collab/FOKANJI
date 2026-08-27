c3c8b2e5e49fe71d4f5531223a61fd9c0f3b9be9
antonioguba90-collab <antonio.gu.ba.90@gmail.com>
2026-08-27 11:57:06 +0200

    Update config.js

--- modulos/config.js
+++ modulos/config.js
@@ -99,7 +99,7 @@ export function resize() {
   ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
 
  if (state.player) {
-    state.player.y = state.H - 25;
+    state.player.y = state.H - 30;
     state.player.x = state.W / 2;
   }
 }
